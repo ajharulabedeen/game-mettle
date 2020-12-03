faf0bc64cdc58b81be69efdd4de5e5b48629d0b8
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-12-04 02:35:16 +0600

    solve image not found problem!

--- src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
+++ src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
@@ -26,11 +26,15 @@ public class MettleGameController {
     private void image_11ClickAction() throws Exception {
         System.out.println("CLikced on Image 11!" + image_1_1.toString());
         String imageName = Utils.imageName.get(rand.nextInt(Utils.imageName.size() - 1));
-//        Image image = new Image("images/" + "cicrle_1.1.png");
-        Image image = new Image("images/" + imageName);
-        System.out.println("Image Name : " + imageName);
-        ImageView imageView = new ImageView(image);
-        image_1_1.setImage(image);
+        try {
+            Image image = new Image("images/" + imageName);
+//            Image image = new Image("images/cicrle_1.8.png");
+            ImageView imageView = new ImageView(image);
+            image_1_1.setImage(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("imageNameError : " + imageName);
+        }
     }
 
 }
