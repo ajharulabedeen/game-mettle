71f24e35de7486cbee5c0ded2fd4c91add0713a3
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-12-04 02:19:38 +0600

    random image show on click, but exception!

--- src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
+++ src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
@@ -25,14 +25,10 @@ public class MettleGameController {
     @FXML
     private void image_11ClickAction() throws Exception {
         System.out.println("CLikced on Image 11!" + image_1_1.toString());
-        Image image = new Image("images\\cicrle_1.1.png");
-//        Image image = new Image("..\\..\\..\\images\\block.png");
-//        String filePath = "..\\..\\..\\images\\block.png";
-//        String filePath = "..\..\..\images\block.png";
-//        String filePath = "images/cicrle_1.1.png";
-        String filePath = "cicrle_1.1.png";
-        FileInputStream input = new FileInputStream(filePath);
-//        Image image = new Image(input);
+        String imageName = Utils.imageName.get(rand.nextInt(Utils.imageName.size() - 1));
+//        Image image = new Image("images/" + "cicrle_1.1.png");
+        Image image = new Image("images/" + imageName);
+        System.out.println("Image Name : " + imageName);
         ImageView imageView = new ImageView(image);
         image_1_1.setImage(image);
     }
