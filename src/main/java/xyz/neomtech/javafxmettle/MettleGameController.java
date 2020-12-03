40918c29153b9b9d7c712a7328aa898ad8406b2a
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-12-04 03:14:17 +0600

    id name fix in FXML file!

--- src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
+++ src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
@@ -2,19 +2,30 @@ package xyz.neomtech.javafxmettle;
 
 import java.io.FileInputStream;
 import java.util.Random;
+
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import xyz.neomtech.javafxmettle.utils.Utils;
 
 public class MettleGameController {
 
     Random rand = new Random();
 
+    //    row one.
     @FXML
     private ImageView image_1_1;
+    @FXML
+    private ImageView image_1_2;
+    @FXML
+    private ImageView image_1_3;
+    @FXML
+    private ImageView image_1_4;
+    @FXML
+    private ImageView image_1_5;
 
-//    @FXML
+    //    @FXML
 //    private ImageView e2b_imageView_to_show_mneaing;
     @FXML
     private void submitButtonAction() {
@@ -23,9 +34,13 @@ public class MettleGameController {
     }
 
     @FXML
-    private void image_11ClickAction() throws Exception {
+    private void image_11ClickAction(MouseEvent me) throws Exception {
         System.out.println("CLikced on Image 11!" + image_1_1.toString());
+//        ImageView iv = (ImageView) me.getSource();
+//        System.out.println(iv.getId());
+        
         String imageName = Utils.imageName.get(rand.nextInt(Utils.imageName.size() - 1));
+
         try {
             Image image = new Image("images/" + imageName);
 //            Image image = new Image("images/cicrle_1.8.png");
