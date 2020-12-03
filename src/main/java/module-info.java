d3281907c7febbba1cba80e4187a0ca62c6b37c3
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-12-03 09:43:32 +0600

    init commit : UI with Block Q Mark!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module xyz.neomtech.javafxmettle {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens xyz.neomtech.javafxmettle to javafx.fxml;
+    exports xyz.neomtech.javafxmettle;
+}
+*/
