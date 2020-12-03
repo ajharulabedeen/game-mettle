e68a9190819637e7a6ef8ca2c79833c07cd3317c
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-12-04 01:07:10 +0600

    Controller working test dummy!

--- src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
+++ src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
@@ -2,7 +2,16 @@ package xyz.neomtech.javafxmettle;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import xyz.neomtech.javafxmettle.utils.Utils;
 
 public class MettleGameController {
+
     
+    
+    @FXML
+    private void submitButtonAction() {
+        System.out.println("Button action working!");
+        Utils.printImageName();
+    }
+
 }
