f76aba8511ee6cee7ef42ebd0dfc0206f11954b6
ajharulabedeen <47244467+ajharulabedeen@users.noreply.github.com>
2020-12-04 01:47:34 +0600

    Fixing main class not fond in build!

--- src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
+++ src/main/java/xyz/neomtech/javafxmettle/MettleGameController.java
@@ -1,17 +1,40 @@
 package xyz.neomtech.javafxmettle;
 
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.util.Random;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import xyz.neomtech.javafxmettle.utils.Utils;
 
 public class MettleGameController {
 
-    
-    
+    Random rand = new Random();
+
+    @FXML
+    private ImageView image_1_1;
+
+//    @FXML
+//    private ImageView e2b_imageView_to_show_mneaing;
     @FXML
     private void submitButtonAction() {
         System.out.println("Button action working!");
         Utils.printImageName();
+    }
+
+    @FXML
+    private void image_11ClickAction() throws Exception {
+        System.out.println("CLikced on Image 11!" + image_1_1.toString());
+        Image image = new Image("images\\cicrle_1.1.png");
+//        Image image = new Image("..\\..\\..\\images\\block.png");
+//        String filePath = "..\\..\\..\\images\\block.png";
+//        String filePath = "..\..\..\images\block.png";
+//        String filePath = "images/cicrle_1.1.png";
+        String filePath = "cicrle_1.1.png";
+        FileInputStream input = new FileInputStream(filePath);
+//        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+        image_1_1.setImage(image);
     }
 
 }
