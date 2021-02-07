76349382b8b6f13f3ca7b75de16a2e4b40913309
ajharulabedeen <ajharulabedeen@gmail.com>
2021-02-07 17:49:33 +0600

    pom changed, working!

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-/* doesn't work with source level 1.8:
-module xyz.neomtech.javafxmettle {
-    requires javafx.controls;
-    requires javafx.fxml;
+// doesn't work with source level 1.8:
+//module xyz.neomtech.javafxmettle {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//
+//    opens xyz.neomtech.javafxmettle to javafx.fxml;
+//    exports xyz.neomtech.javafxmettle;
+//}
 
-    opens xyz.neomtech.javafxmettle to javafx.fxml;
-    exports xyz.neomtech.javafxmettle;
-}
-*/
